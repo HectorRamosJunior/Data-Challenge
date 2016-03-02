3004d5b41135255d6002cd4aa39e79a39e205386
Hector Ramos <HectorRamos.Junior@gmail.com>
2016-03-02 18:01:00 -0500

    First part finished

--- Part1.py
+++ Part1.py
@@ -10,26 +10,43 @@ def makeSolution(inputFileName, outputFileName):
 
     #Dict reader/writer for flexibility with possible column changes
     reader = csv.DictReader(open(inputFileName))
+
+    writerHeader = reader.fieldnames
+    writerHeader.append("start_date_description")
     writer = csv.DictWriter(open(outputFileName,'wb'), 
-                                fieldnames = reader.fieldnames)
+                                fieldnames = writerHeader)
     writer.writeheader()
+
 
     #O(1) readtime instead of reading from the csv file each time
     stateDict = getStateDict("state_abbreviations.csv")
+
+    #List initiatilized above dateOffset() scope for slight efficiency
+    #A set could have been used instead, however the scope is small
+    #And the list holds the order of the months by index
+    monthList = ["January", "February", "March", "April", "May", "June", "July", 
+                    "August", "September", "October", "November", "December"]
+
 
     #Write the rows to the new file, fixing them along the way
     for row in reader:
         fixedRow = row 
 		
-		#Fix the dictionary fields as desired
+		#Fixes the bio field, changes state abrev to full name using dict
         fixedRow["bio"] = " ".join(fixedRow["bio"].split())
         if fixedRow["state"] in stateDict:
             fixedRow["state"] = stateDict[fixedRow["state"]]
+
+        #Sets the start date fields from the tuple in the function    
+        date, date_description = dateOffset(fixedRow["start_date"], monthList)
+        fixedRow["start_date"] = date 
+        fixedRow["start_date_description"] = date_description
 
 		#Write the modified dictionary as a new row to the output csv
         writer.writerow(fixedRow)
 
 
+#Returns a dictionary of the state abbreviations for O(1) runtime
 def getStateDict(fileName):
     reader = csv.reader(open(fileName, "rb"))
     stateDict = {}
@@ -44,6 +61,54 @@ def getStateDict(fileName):
         stateDict[row[0]] = row[1]
 
     return stateDict
+
+#Returns a tuple for the start date fields, catches errors in start date
+def dateOffset(date, monthList):
+    spaced = date.split()
+    slashed = date.split("/")
+
+    #To be normalized to "YYYY-MM-DD"
+    normalized = "" 
+
+    if len(spaced) == 3:
+        #Valid Dates to be normalized: "June 26, 1977"  
+        
+        #YYYY-
+        if spaced[2].isdigit():
+            normalized += spaced[2] + "-"
+        
+        #MM-
+        for i in xrange(len(monthList)):
+            if spaced[0] == monthList[i]:
+                if i+1 < 10:
+                    normalized += "0"
+                normalized += str(i+1) + "-"    
+
+        #DD
+        spaced[1] = spaced[1].replace(",", "")
+        if spaced[1].isdigit():
+            normalized += spaced[1]
+
+    if len(slashed) == 3:
+        #Valid Dates to be normalized: "10/27/1998"
+        
+        #YYYY-
+        if slashed[2].isdigit():
+            normalized += slashed[2] + "-"
+        
+        #MM-
+        if slashed[0].isdigit():
+            normalized += slashed[0] + "-"   
+
+        #DD
+        if slashed[1].isdigit():
+            normalized += slashed[1]
+
+
+    #Checks if date was properly normalized
+    if len(normalized) != 10:
+        return "Invalid", date
+    return normalized, ""
 
 
 makeSolution("test.csv", "solution.csv")
