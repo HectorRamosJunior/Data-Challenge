e431c5f950ebcb6e88e3da24e1e39c56c10cf6ae
Hector Ramos <HectorRamos.Junior@gmail.com>
2016-03-02 23:55:17 -0500

    Documentation fixing

--- Part Two/Part2.py
+++ Part Two/Part2.py
@@ -1,42 +1,48 @@
-#Hector Ramos
-#3/2/2016
-#Enigma Coding Challenge Part 2
+""" Hector Ramos
+3/2/2016
+Enigma Coding Challenge Part 2
+"""
 import json, urllib
 from bs4 import BeautifulSoup
 
-
 def scrapeData(mainURL):
+    """
+        Iterates through the pages of the mainurl to get the 
+        company url/data
+    """
     companyDict = {}
-    next = "" #placeholder for iterating through the pages
+    next = "" # placeholder for iterating through the pages
 
     while True:
         page = urllib.urlopen(mainURL + next).read()
         soup = BeautifulSoup(page, "html.parser")
 
-        #Parses for all the companies on the webpage and the urls
-        table = soup.find("table")
-        companyURLs = table.findAll("a")
+        # Parses for all the companies on the webpage and the urls
+        companyPageURLs = soup.find("table").findAll("a")
 
-        for a in companyURLs:
-            name = str(a.text.lstrip()) #Unicode object
+        for a in companyPageURLs:
+            name = str(a.text.lstrip()) # Unicode object
             data = getCompanyData(mainURL + a["href"])
 
-            #Updates dictionary with the new company entry
+            # Updates dictionary with the new company entry
             companyDict[name] = data 
 
-        #Checks if the next page tag doesn't exist.
+        # Checks if the next page tag doesn't exist.
         if soup.find("li", class_="next disabled"):
             break
 
-        #If the next page exists, move onto the next page
+        # If the next page exists, move onto the next page
         next = soup.find("li", class_= "next").find("a").get("href")
         print "Moving onto page " + mainURL + next
 
     json.dump(companyDict, open("solution.json", "w"))
 
 
-#returns dictionary of parsed data from the company page
+# Returns dictionary of parsed data from the company page
 def getCompanyData(companyURL):
+    """
+        Returns dictionary of parsed data from the company page
+    """
     page = urllib.urlopen(companyURL).read()
     soup = BeautifulSoup(page, "html.parser")
 
@@ -45,7 +51,7 @@ def getCompanyData(companyURL):
     tds = soup.findAll("td")
     for td in tds:
         if td.has_attr("id"):
-            dataDict[str(td.get("id"))] = str(td.text) #unicode objects
+            dataDict[str(td.get("id"))] = str(td.text) # Unicode objects
     
     return dataDict
 
